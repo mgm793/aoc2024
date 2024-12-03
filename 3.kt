179b19b3d3d375da23592a05c7c6611e273aef9b
Marc <mgm793@gmail.com>
2024-12-03 10:39:31 +0100

    3 day second part

--- 3.kt
+++ 3.kt
@@ -10,8 +10,36 @@ fun firstPart(text: String): Long{
   }
 }
 
+
+fun secondPart(text: String): Long{
+  val regex = Regex("""mul\(\d+,\d+\)|do\(\)|don't\(\)""")
+  val matches = regex.findAll(text)
+  var isEnabled = true
+  return matches.fold(0L){acc: Long, match ->
+    val value: String = match.value
+    when(value){
+      "don't()" -> {
+        isEnabled = false
+        acc
+      }
+      "do()" -> {
+        isEnabled = true
+        acc
+      }
+      else -> {
+        var newValue = acc
+        if(isEnabled) {
+          val values = value.replace("""mul\(|\)""".toRegex(), "").split(",")
+          newValue += (values[0].toLong() * values[1].toLong())
+        }
+        newValue
+      }
+    } 
+  }
+}
+
 fun main(){
   val text = File("./input_3.txt").readText()
   println(firstPart(text))
-
+  println(secondPart(text))
 }
